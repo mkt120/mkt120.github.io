6c297871db2ecfa2b8881f291878a92fc157f295
dotcourt <dev.mkt120.elop@gmail.com>
2025-06-06 10:58:06 +0900

    いったん

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,3 +11,11 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-04-03',
 })
+
+// 以下が参考になりそう
+// https://beyooon.jp/blog/nuxt-ssg-generate-routes-trouble/
+async function route() {
+  const { $content } = require('@nuxt/content');
+  const dynamicRoutes = await $content('blog').fetch();
+  return dynamicRoutes.map(myroute => myroute.slug === '/index' ? '/' : '/blog/' + myroute.slug);
+}
