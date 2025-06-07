c6bbc20494106a4b5d17dcc6e0c9811855674a9f
dotcourt <dev.mkt120.elop@gmail.com>
2025-06-07 17:49:54 +0900

    index.vueを更新。

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -12,14 +12,12 @@ const { data } = await useAsyncData(() => {
 <template>
   <div v-if="data" v-for="item in data" class="list">
     <h4>{{ item.title }}</h4>
-    <ul v-if="item.children">
-      <li v-for="article in item.children">
-        <li v-if="article" :key="article.path" class="post-item">
-          <!-- 記事一覧の生成 -->
-          <NuxtLink :to="article.path">
-            <div>{{ article.title }}</div>
-          </NuxtLink>
-        </li>
+    <ul v-if="item.children" v-for="article in item.children">
+      <li v-if="article" :key="article.path" class="post-item">
+        <!-- 記事一覧の生成 -->
+        <NuxtLink :to="article.path">
+          <div>{{ article.title }}</div>
+        </NuxtLink>
       </li>
     </ul>
   </div>
