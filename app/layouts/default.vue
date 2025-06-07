8233861138356920eb0338004459974bd7fd1b94
dotcourt <dev.mkt120.elop@gmail.com>
2025-06-07 17:47:10 +0900

    format

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -31,113 +31,107 @@ export default {
 
 
 
-.content {
+  .content {
 
-  color: black;
+    color: black;
 
-  h1 {
-    /** position */
-    margin-top: 1.6em;
-    margin-bottom: 12px;
+    h1 {
+      /** position */
+      margin-top: 1.6em;
+      margin-bottom: 12px;
 
-    font-size: 20px;
+      font-size: 20px;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+        /* 親要素のテキスト色を継承する */
+      }
+    }
+
+    h2 {
+      /** position */
+      margin-top: 1.6em;
+
+      font-size: 18px;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+        /* 親要素のテキスト色を継承する */
+      }
+
+    }
+
+    h3 {
+      /** position */
+      margin-top: 1.2em;
+
+      font-size: 16px;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+        /* 親要素のテキスト色を継承する */
+      }
+    }
+
+    h4 {
+      /** position */
+      margin-top: 0.8em;
+
+      font-size: 16px;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+        /* 親要素のテキスト色を継承する */
+      }
+    }
+
+    p {
+      /** position */
+      margin-left: 12px;
+
+      font-size: 16px;
+      line-height: 1.5em;
+    }
+
+    p code {
+      padding: 4px;
+      background-color: gray;
+      border: 1px;
+      border-radius: 5px;
+      border-color: white;
+      font-size: 14px;
+      margin-left: 4px;
+      margin-right: 4px;
+    }
+
+    pre {
+      padding: 12px;
+      font-size: 16px;
+      line-height: 1.5em;
+      background-color: white;
+    }
+
+    div {
+      /** position */
+      margin-left: 12px;
+    }
+
+    li {
+      margin-top: 0.4em;
+      margin-bottom: 0.4em;
+    }
 
     a {
       text-decoration: none;
-      color: inherit;
-      /* 親要素のテキスト色を継承する */
-    }
-  }
-
-  h2 {
-    /** position */
-    margin-top: 1.6em;
-
-    font-size: 18px;
-
-    a {
-      text-decoration: none;
-      color: inherit;
-      /* 親要素のテキスト色を継承する */
     }
 
-  }
-
-  h3 {
-    /** position */
-    margin-top: 1.2em;
-
-    font-size: 16px;
-
-    a {
-      text-decoration: none;
-      color: inherit;
-      /* 親要素のテキスト色を継承する */
+    a:hover {
+      text-decoration: underline;
     }
   }
-
-  h4 {
-    /** position */
-    margin-top: 0.8em;
-
-    font-size: 16px;
-
-    a {
-      text-decoration: none;
-      color: inherit;
-      /* 親要素のテキスト色を継承する */
-    }
-  }
-
-  p {
-    /** position */
-    margin-left: 12px;
-
-    font-size: 16px;
-    line-height: 1.5em;
-  }
-
-  p code {
-    padding: 4px;
-    background-color: gray;
-    border: 1px;
-    border-radius: 5px;
-    border-color: white;
-    font-size: 14px;
-    margin-left: 4px;
-    margin-right: 4px;
-  }
-
-  pre {
-    padding: 12px;
-    font-size: 16px;
-    line-height: 1.5em;
-    background-color: white;
-  }
-
-  div {
-    /** position */
-    margin-left: 12px;
-  }
-
-  li {
-    margin-top: 0.4em;
-    margin-bottom: 0.4em;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  a:hover {
-    text-decoration: underline;
-  }
-}
-
-
-
-
-
-
 }
 </style>
