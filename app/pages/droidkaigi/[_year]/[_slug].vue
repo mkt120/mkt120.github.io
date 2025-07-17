bc5f2f641fa8841aafbc3dd489c6a9acac912f91
dotcourt <dev.mkt120.elop@gmail.com>
2025-07-18 06:23:41 +0900

    update

---
+++ app/pages/droidkaigi/[_year]/[_slug].vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import convertDate  from '~/pages/date.js'
+
+const route = useRoute()
+
+console.log("route:" + route.path)
+const { data: post } = await useAsyncData(route.path, () => {
+  return queryCollection('blog')
+    .path(route.path).first()
+})
+
+useHead({
+  title: post.value?.title + " | どっこと備忘録群"
+})
+</script>
+
+<template>
+  <div v-if="post">
+    <h1>{{ post.title }}</h1>
+    <ContentRenderer :value="post" />
+    <div class="last_update">
+      <p>最終更新: {{ convertDate(post.date) }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.last_update {
+  margin-top: 4.0em;
+}
+</style>
---
+++ app/pages/droidkaigi/[_year]/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { ContentNavigationItem } from '@nuxt/content'
+
+
+const route = useRoute()
+
+console.log("route:" + route.path)
+const { data } = await useAsyncData(() => {
+  return queryCollectionNavigation("blog", ["draft"])
+    .where("draft", "=", false)
+    .where("path", "LIKE", route.path + "%")
+})
+
+useHead({
+  title: data.value ? data.value[0]?.title + " | どっこと備忘録群" : "どっこと備忘録群",
+})
+
+</script>
+
+<template>
+  <div v-if="data" v-for="item in data">
+    <h1>{{ item.title }}</h1>
+    <div v-if="item.children" v-for="article in item.children">
+      <h2 class="post-item">{{ article.title }}</h2>
+        <!-- 記事一覧の生成 -->
+      <ul v-if="article.children" v-for="item in article.children">
+      <NuxtLink :to="item.path">
+        <li>{{ item.title }}</li>
+      </NuxtLink>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style></style>
