d79bab9db4551c00a65512e600883051b68ad016
dotcourt <dev.mkt120.elop@gmail.com>
2026-08-22 23:31:40 +0900

    revert date.js

---
+++ app/pages/date.js
@@ -0,0 +1,10 @@
+export default function convertDate(dateString) {
+    let date = new Date(dateString);
+    return `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
+}
+
+export function convertFullDate(dateString) {
+    let date = new Date(dateString);
+    let minute = String(date.getMinutes()).padStart(2, '0');
+    return `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()} ${date.getHours()}:${minute}`;
+}
